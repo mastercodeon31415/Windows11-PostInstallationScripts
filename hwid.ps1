6012379ffa9e2c3e86c016740937e4c3fe3d4914
0% [█ █ █ █ █ █ █ █ █ █] 100% <mastercodeon@gmail.com>
2025-08-12 03:19:17 -0500

    Update hwid.ps1

--- hwid.ps1
+++ hwid.ps1
@@ -1,2 +1,4 @@
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mastercodeon31415/Windows11-PostInstallationScripts/refs/heads/main/hwidScript.b64', [System.Environment]::ExpandEnvironmentVariables("%TEMP%") + '\temp.b64')"
-cmd -c certutil -f -decode %temp%\temp.b64 %temp%\decoded.bat > nul && cmd -c call %temp%\decoded.bat && cmd -c del %temp%\temp.b64 %temp%\decoded.bat
+cmd -c certutil -f -decode %temp%\temp.b64 %temp%\decoded.bat > nul 
+cmd -c call %temp%\decoded.bat 
+cmd -c del %temp%\temp.b64 %temp%\decoded.bat
